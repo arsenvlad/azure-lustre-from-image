6433c43fd1dc411384948b724dbde6df103f7281
Arsen Vladimirskiy <arsenvlad@users.noreply.github.com>
2017-07-07 10:49:56 -0500

    Use latest apiVersions, fault domain counts, and var disk count.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Deploying Lustre cluster using managed disks and image created from blob
+# Deploy Lustre 2.7 cluster using managed disks and image created from blob
+
+The blob VHD image must be in your storage account within the same subscription and region/location where you are deploying the Lustre cluster. The template will create a managed image from the blob VHD and will use the created managed image to create the VMs.
--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,12 @@
+# Azure PowerShell
 Login-AzureRmAccount
-
 Get-AzureRmSubscription
 Select-AzureRmSubscription -SubscriptionName "Arsen Internal"
-
 New-AzureRmResourceGroup -Name "avlustre1" -Location "westus"
-
 New-AzureRmResourceGroupDeployment -ResourceGroupName "avlustre1" -TemplateFile "mainTemplate.json" -TemplateParameterFile "mainTemplate.parameters.json" -vnetResourceGroup "avlustre1" -publicIPAddressResourceGroup "avlustre1" -imageResourceGroup "avlustre1" -imageNewOrExisting new
 
-
+# Azure CLI 2.0
+az account set --subscription "Arsen Internal"
+az group list
+az group create --location "westus" --name "avlustre1"
+az group deployment create --resource-group "avlustre1" --template-file "mainTemplate.json" --parameters @mainTemplate.parameters.json --parameters ossDiskCount=5
